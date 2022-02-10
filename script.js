8fcc956b467b18c330ecb5e24be19f0a7297903a
Franz <FranzFischbach@gmail.com>
2022-02-10 15:41:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ function moveLeft(){
 }
 function moveRight(){
     var left = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
+    var left = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
     if(left<380){
         character.style.left = left + 2 + "px";
     }
